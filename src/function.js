a49fd33c252fd9a89cf369f0ccf3351fd1e57684
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-02-27 21:11:26 +0300

    correction  27.02

--- src/function.js
+++ src/function.js
@@ -1,26 +1,36 @@
 export default class Team {
-  constructor() {  
-  this.hero = [
-    {name: 'Лучник', health: 50, level: 1, type: 'Bowman', attack: 25, defence: 25},
-    {name: 'Мечник', health: 50, level: 1, type: 'Swordsman', attack: 40, defence: 10},
-    {name: 'Безсмертный', health: 50, level: 1, type: 'Undead', attack: 10, defence: 40},
-    {name: 'Маг', health: 50, level: 1, type: 'Magician', attack: 25, defence: 25}
-  ]
-}
-
-[Symbol.iterator](){
-  let hero = this.hero;    
-  let current = 0;    
-  return {next(){        
-      if(current < hero.length){
-          let done = false;
-              let value = hero[current];
-          console.log(value);
-          current++;
-              return {
-              done, value
-          }} else{return {done: true}}
-      }
+  constructor() {
+    this.hero = [
+      {
+        name: 'Лучник', health: 50, level: 1, type: 'Bowman', attack: 25, defence: 25,
+      },
+      {
+        name: 'Мечник', health: 50, level: 1, type: 'Swordsman', attack: 40, defence: 10,
+      },
+      {
+        name: 'Безсмертный', health: 50, level: 1, type: 'Undead', attack: 10, defence: 40,
+      },
+      {
+        name: 'Маг', health: 50, level: 1, type: 'Magician', attack: 25, defence: 25,
+      },
+    ];
   }
-}
+
+  [Symbol.iterator]() {
+    const hero = this.hero;
+    let current = 0;
+    return {
+      next() {
+        if (current < hero.length) {
+          const done = false;
+          const value = hero[current];
+          console.log(value);
+          current += 1;
+          return {
+            done, value,
+          };
+        } return { done: true };
+      },
+    };
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,39 +1,47 @@
 import Team from '../src/function';
 
-const team = new Team;
-let it = team[Symbol.iterator]();
+const team = new Team();
+const it = team[Symbol.iterator]();
 
 test('Первый игрок', () => {
-  const expected ={name: 'Лучник', health: 50, level: 1, type: 'Bowman', attack: 25, defence: 25};
+  const expected = {
+    name: 'Лучник', health: 50, level: 1, type: 'Bowman', attack: 25, defence: 25,
+  };
   const hero = it.next();
   const received = hero.value;
   expect(received).toEqual(expected);
 });
 
 test('Второй игрок', () => {
-  const expected ={name: 'Мечник', health: 50, level: 1, type: 'Swordsman', attack: 40, defence: 10};
+  const expected = {
+    name: 'Мечник', health: 50, level: 1, type: 'Swordsman', attack: 40, defence: 10,
+  };
   const hero = it.next();
   const received = hero.value;
   expect(received).toEqual(expected);
 });
 
 test('Третий игрок', () => {
-  const expected ={name: 'Безсмертный', health: 50, level: 1, type: 'Undead', attack: 10, defence: 40};
+  const expected = {
+    name: 'Безсмертный', health: 50, level: 1, type: 'Undead', attack: 10, defence: 40,
+  };
   const hero = it.next();
   const received = hero.value;
   expect(received).toEqual(expected);
 });
 
 test('Четвертый игрок', () => {
-  const expected ={name: 'Маг', health: 50, level: 1, type: 'Magician', attack: 25, defence: 25};
+  const expected = {
+    name: 'Маг', health: 50, level: 1, type: 'Magician', attack: 25, defence: 25,
+  };
   const hero = it.next();
   const received = hero.value;
   expect(received).toEqual(expected);
 });
 
 test('Пятый игрок', () => {
-  const expected =true;  
+  const expected = true;
   const hero = it.next();
   const received = hero.done;
   expect(received).toEqual(expected);
-}); 
+});
